aad7b50428533b062ded0e2e8d1a7756f9f93b04
bebe228 <Andriu.botok@gmail.com>
2024-10-24 23:53:19 +0300

    Main_LW_1_5.cs

--- MAC_LabWork_1_5/Main_LW_1_5.cs
+++ MAC_LabWork_1_5/Main_LW_1_5.cs
@@ -56,46 +56,5 @@ namespace MAC_LabWork_1_5
         }
             
             
-            
-            
-            
-            #region LW_1_5
-        //    using (StreamWriter SW = UTL.ResultWriter("LW_1_5.txt"))
-        //    {
-        //        MyToF T_Fx = new MyToF(1.0, 10.0, 500, Fx, "Fx");
-        //        T_Fx.Roots_Correction(eps);
-        //        SW.WriteLine(T_Fx.Table_of_Roots("- Dichotomy -"));
-
-        //        double xr = double.NaN;
-        //        SW.WriteLine("\r\n Таблиця Нулів, яку обчислено за схемою (1.5.1):");
-        //        for (int j = 0; j < T_Fx.Roots.Count; j++)
-        //        {
-        //            xr = Eq.Tangent(Fx, D1F, D2F, T_Fx.Roots[j].XL, T_Fx.Roots[j].XR, eps, out int K);
-        //            SW.WriteLine($"{j,3}{xr,17:F12}{Math.Abs(Fx(xr)), 10:E1}{K,3}");
-        //        }
-        //        SW.WriteLine("\r\n Таблиця нулів, яку обчислено за схемою (1.5.2):");
-        //        for (int j = 0; j < T_Fx.Roots.Count; j++)
-        //        {
-        //            xr = Eq.Tangent(Fx, T_Fx.Roots[j].XL, T_Fx.Roots[j].XR, eps, out int K);
-        //            SW.WriteLine($"{j,3}{xr,17:F12}{Math.Abs(Fx(xr)),10:E1}{K,3}");
-        //        }
-        //    }
-        //}
-        
-        //public static double Fx(double x)
-        //{
-        //    return Math.Tanh(x) - 2.0 * Math.Cos(Sq10 * x);
-        //}
-
-        //public static double D1F(double x)
-        //{
-        //    return 1.0 / Math.Cosh(x) / Math.Cosh(x) + 2.0 * Sq10 * Math.Sin(Sq10 * x);
-        //}
-        //public static double D2F(double x)
-        //{
-        //    return 2.0 * (10.0 * Math.Cos(Sq10 * x) - Math.Tanh(x) / Math.Cosh(x) / Math.Cosh(x));
-        //}
-        #endregion
-
     }
 }
